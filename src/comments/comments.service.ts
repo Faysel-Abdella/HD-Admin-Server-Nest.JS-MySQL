1c14e3ae3f57c4365c869cc0982cf155e1d68165
Faysel Abdella <fayselcode@gmail.com>
2024-07-08 07:17:14 +0300

    Add try-catch block for all

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -8,6 +8,18 @@ export class CommentsService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createCommentDto: CreateCommentDto) {
+    const review = await this.prisma.review.findUnique({
+      where: { review_id: createCommentDto.review_id },
+    });
+
+    if (!review) {
+      return {
+        statusCode: HttpStatus.NOT_FOUND,
+        message:
+          'The review to add the comment is not found (Invalid review id)',
+      };
+    }
+
     try {
       const comment = await this.prisma.comment.create({
         data: createCommentDto,
@@ -19,46 +31,60 @@ export class CommentsService {
         data: comment,
       };
     } catch (error) {
-      console.log('Failed to add a comment');
       return {
-        statusCode: HttpStatus.NOT_FOUND,
-        message: 'Failed to add a comment (probably Invalid review_id)',
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
       };
     }
   }
 
   async findAll(page?: number, limit?: number) {
     const skip = page ? (page - 1) * limit : 0;
-    const comments = await this.prisma.comment.findMany({
-      skip: skip,
-      ...(limit && { take: limit }),
-    });
 
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'Comments retrieved successfully',
-      data: comments,
-    };
+    try {
+      const comments = await this.prisma.comment.findMany({
+        skip: skip,
+        ...(limit && { take: limit }),
+      });
+
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Comments retrieved successfully',
+        data: comments,
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      };
+    }
   }
 
   async findOne(id: number) {
-    const comment = await this.prisma.comment.findUnique({
-      where: { comment_id: id },
-    });
+    try {
+      const comment = await this.prisma.comment.findUnique({
+        where: { comment_id: id },
+      });
 
-    if (!comment) {
+      if (!comment) {
+        return {
+          statusCode: HttpStatus.NOT_FOUND,
+          message: 'Comment not found',
+          data: comment,
+        };
+      }
+
       return {
-        statusCode: HttpStatus.NOT_FOUND,
-        message: 'Comment not found',
+        statusCode: HttpStatus.OK,
+        message: 'Comment retrieved successfully',
         data: comment,
       };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      };
     }
-
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'Comment retrieved successfully',
-      data: comment,
-    };
   }
 
   async update(id: number, updateCommentDto: UpdateCommentDto) {
@@ -72,20 +98,26 @@ export class CommentsService {
         message: 'Comment not found (Invalid Id)',
       };
     }
+    try {
+      const comment = await this.prisma.comment.update({
+        where: { comment_id: id },
+        data: {
+          ...updatedComment,
+          ...updateCommentDto,
+        },
+      });
 
-    const comment = await this.prisma.comment.update({
-      where: { comment_id: id },
-      data: {
-        ...updatedComment,
-        ...updateCommentDto,
-      },
-    });
-
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'Comment updated successfully',
-      data: comment,
-    };
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Comment updated successfully',
+        data: comment,
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      };
+    }
   }
 
   async remove(id: number) {
@@ -100,14 +132,21 @@ export class CommentsService {
       };
     }
 
-    const comment = await this.prisma.comment.delete({
-      where: { comment_id: id },
-    });
+    try {
+      const comment = await this.prisma.comment.delete({
+        where: { comment_id: id },
+      });
 
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'Comment deleted successfully',
-      data: comment,
-    };
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Comment deleted successfully',
+        data: comment,
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      };
+    }
   }
 }
